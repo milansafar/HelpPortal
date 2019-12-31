b59f3fbb12deb65288dd2f5f98255e23c5c829e3
milansafar <safar.mi@gmail.com>
2019-12-31 18:05:48 +0100

    Base database model for answer with sequelize

---
+++ db/answer.js
@@ -0,0 +1,15 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('./db')
+
+class Answer extends Model {}
+
+Answer.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING
+}, {
+    sequelize, modelName: 'answer'
+});
+
+sequelize.sync()
+
+module.exports = Answer
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var answer = require('../db/answer')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  answer.findAll().then(answers => {
+    res.render('index', { title: 'Customer Portal', answers: answers});
+  });
+});
 
+router.get('/create', function (req, res, next) {
+  answer.create({title: "title", description: "desription"});
 
-  res.render('index', { title: 'Customer Portal'});
+  res.redirect('/');
 });
 
 module.exports = router;
