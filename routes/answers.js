801c2fb95bd17c18a10440d12ba034f38d71aca3
milansafar <safar.mi@gmail.com>
2020-01-04 00:20:30 +0100

    Add csfr tokens and validation for fields, form error message

--- routes/answers.js
+++ routes/answers.js
@@ -1,14 +1,56 @@
 var express = require('express');
 var router = express.Router();
 var answer = require('../db/answer')
+const { check, validationResult } = require('express-validator');
+var csrf = require('csurf')
 
-router.get('/add', function (req, res, next) {
-    res.render('answers/add', { title: 'Add Answer'});
-});
+// setup route middlewares
+var csrfProtection = csrf({ cookie: true })
 
-router.post('/', function (req, res, next) {
-  answer.create({title: req.body.title , description: req.body.description});
-  res.redirect('/');
-});
+router.get(
+    '/add', 
+    csrfProtection, 
+    function (req, res, next) {
+        res.render(
+            'answers/add', 
+            { 
+                title: 'Add Answer', 
+                csrfToken: req.csrfToken()
+            }
+        );
+    }
+);
+
+router.post(
+    '/', 
+    [
+        check('title')
+            .not().isEmpty()
+            .trim().withMessage('Title must be filled'),
+        check('description')
+            .not().isEmpty()
+            .trim().withMessage('Description must be filled'),
+    ],
+
+    csrfProtection,
+
+    function (req, res, next) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+
+            res.render(
+                'answers/add', 
+                { 
+                    title: 'Add Answer', 
+                    csrfToken: req.csrfToken(),
+                    errors: errors.array()
+                }
+            );
+        } else {
+            answer.create({title: req.body.title , description: req.body.description});
+            res.redirect('/'); 
+        }
+    }
+);
 
 module.exports = router;
